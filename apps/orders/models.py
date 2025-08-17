9e8322cbdbb65dc32a4213a58e65950ce798ee29
medineearyal <medineearyal@gmail.com>
2025-08-17 21:47:01 +0545

    Remove Order Category

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,15 +1,10 @@
 from django.db import models
 from django.utils import timezone
 
+from apps.products.models import Category
+
 
 # Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-
 class Order(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
