c64b8d105a9e2b2bb988392c864391ec61083611
medineearyal <medineearyal@gmail.com>
2025-09-04 04:25:03 +0545

    Fix Categories

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -32,7 +32,7 @@ class HomePageView(TemplateView):
         context = super(HomePageView, self).get_context_data(**kwargs)
 
         banners = ProductBanner.objects.filter(is_display=True)[:4]
-        categories = Category.objects.annotate(product_count=Count("product_category", distinct=True)).filter(parent__isnull=True, product_count__gt=0)
+        categories = Category.objects.annotate(product_count=Count("product_category", distinct=True)).filter(parent__isnull=True)
 
         products = Product.objects.all().filter(is_verified=True)
 
